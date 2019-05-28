6a5e2b30ea625a7ca0419881804ed20e0c6f1bd9
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-05-28 14:40:58 -0700

    Achieved contact with API

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    var getRandJokeBtn = document.querySelector("main > button");
+    getRandJokeBtn.onclick = getRandomJoke;
+};
+function getRandomJoke() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = handleRequestChange;
+    var url = "http://api.icndb.com/jokes/random";
+    request.open("GET", url);
+    request.send();
+}
+function handleRequestChange() {
+    var currRequest = this;
+    console.log("ReadyState: " + currRequest.readyState);
+    console.log("Status: " + currRequest.status);
+    if (currRequest.readyState == 4 &&
+        currRequest.status == 200) {
+        var response = currRequest.responseText;
+        alert(response);
+    }
+}
---
+++ typescript/main.ts
@@ -0,0 +1,39 @@
+window.onload = function(){
+    let getRandJokeBtn = <HTMLElement>document.querySelector("main > button");
+    getRandJokeBtn.onclick = getRandomJoke;
+}
+
+/**
+ * Gets a ransom joke from the ICNDB.com service
+ */
+function getRandomJoke(){
+    //create request object
+    let request = new XMLHttpRequest();
+
+    //every time readystate is changed, this function is called 
+    request.onreadystatechange = handleRequestChange;
+
+    //prepare request
+    let url = "http://api.icndb.com/jokes/random";
+    request.open("GET", url); //get is essentially SELECT 
+
+    //send request
+    request.send();
+}
+
+/**
+ * Fires every time the ready state property is changed. 
+ * https://www.w3schools.com/js/js_ajax_http.asp
+ */
+function handleRequestChange(){
+    let currRequest = <XMLHttpRequest>this;
+    console.log("ReadyState: " + currRequest.readyState);
+    console.log("Status: " + currRequest.status);
+
+    //when processing is done and response/data is ready
+    if(currRequest.readyState == 4 && 
+       currRequest.status == 200){
+        let response = currRequest.responseText;
+        alert(response);
+    }
+}
